75be6ca9430b501242fa08737c4b215a5fe37593
Mikrofala54 <cocietoobchodzi326@gmail.com>
2026-05-11 10:06:51 +0200

    Wersja 3

---
+++ kablemorskie/data/cables.js
@@ -0,0 +1,29 @@
+const cables = [
+  {
+    name: "Transatlantic Cable 1",
+    from: "USA (New York)",
+    to: "UK (London)",
+    path: [
+      [40.7128, -74.0060],
+      [50, -30],
+      [51.5074, -0.1278]
+    ],
+    color: "red",
+    length: 6000,
+    capacity: "200 Tbps"
+  },
+
+  {
+    name: "Pacific Link",
+    from: "USA",
+    to: "Japan",
+    path: [
+      [40.7128, -74.0060],
+      [30, -160],
+      [35.6762, 139.6503]
+    ],
+    color: "blue",
+    length: 9000,
+    capacity: "120 Tbps"
+  }
+];
---
+++ kablemorskie/data/cities.js
@@ -0,0 +1,20 @@
+const cities = [
+  {
+    name: "New York",
+    country: "USA",
+    coords: [40.7128, -74.0060],
+    img: "https://source.unsplash.com/400x300/?newyork"
+  },
+  {
+    name: "London",
+    country: "UK",
+    coords: [51.5074, -0.1278],
+    img: "https://source.unsplash.com/400x300/?london"
+  },
+  {
+    name: "Tokyo",
+    country: "Japan",
+    coords: [35.6762, 139.6503],
+    img: "https://source.unsplash.com/400x300/?tokyo"
+  }
+];
